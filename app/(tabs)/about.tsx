4d94d8a6189aa592d1309d95ee3e6c3587663040
AryanMurari <1aryanmurari@gmail.com>
2025-02-06 17:54:21 +0530

    all git test done

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,8 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is About screen. Testing with main branch</Text>
-      <Text style={styles.text}>This is About screen. Now testing with another 3rd branch</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
