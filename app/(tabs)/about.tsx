c7a9df1edb13ccdd2e6da1ecb6dc08b3d54fb0e1
AryanMurari <1aryanmurari@gmail.com>
2025-02-06 17:20:37 +0530

    will this be commited in branch?

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is About screen</Text>
+      <Text style={styles.text}>This is About screen. I will test this for my branch</Text>
     </View>
   );
 }
