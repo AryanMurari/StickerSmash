b67413e208a4c160f84d1e41e5ad274fef377a58
AryanMurari <1aryanmurari@gmail.com>
2025-02-06 17:38:58 +0530

    testing with 3rd branch

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is About screen. Testing with test2branch</Text>
+      <Text style={styles.text}>This is About screen. Now testing with another 3rd branch</Text>
     </View>
   );
 }
